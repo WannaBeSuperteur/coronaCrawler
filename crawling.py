5c48d5336e0511775d29dd991a4d8237c53700ed
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2020-07-08 10:19:04 +0900

    update for July 08 2020

--- crawling.py
+++ crawling.py
@@ -127,7 +127,7 @@ for i in range(len(linkList)): # list.txt 파일에 있는 각 질병관리본�
              or time == '20060400' or time == '20060500' or time == '20060600' or time == '20060700' or time == '20060800'
              or time == '20061100' or time == '20061300' or time == '20061400' or time == '20061500' or time == '20062100'
              or time == '20062200' or time == '20062400' or time == '20062600' or time == '20062700' or time == '20062800'
-             or time == '20062900' or time == '20063000' or time == '20070100' or time == '20070200' or time == '20070300' or time == '20070400'):
+             or time == '20062900' or time == '20063000' or time == '20070100' or time == '20070200' or time == '20070300' or time == '20070400' or time == '20070800'):
 
             # 9~16열: 지역별 격리중  (순서: 전국 서울 부산 대구 인천 광주 대전 울산 세종 경기 강원 충북 충남 전북 전남 경북 경남 제주)
             for j in range(37, 54): resultAppend += str(valueList[j]) + ' '
@@ -275,7 +275,7 @@ for i in range(len(linkList)): # list.txt 파일에 있는 각 질병관리본�
             or time == '20060400' or time == '20060500' or time == '20060600' or time == '20060700' or time == '20060800'
             or time == '20061100' or time == '20061300' or time == '20061400' or time == '20061500' or time == '20062100'
             or time == '20062200' or time == '20062400' or time == '20062600' or time == '20062700' or time == '20062800'
-            or time == '20062900' or time == '20063000' or time == '20070100' or time == '20070200' or time == '20070300' or time == '20070400'):
+            or time == '20062900' or time == '20063000' or time == '20070100' or time == '20070200' or time == '20070300' or time == '20070400' or time == '20070800'):
             
             for j in range(94, 111): resultAppend += str(valueList[j]) + ' '
             resultAppend += str(valueList[111]) + '#'
