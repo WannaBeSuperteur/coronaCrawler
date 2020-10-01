3f37ef2dcc64fbf82028320aafbc108ab3aaa54e
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2020-10-01 15:55:05 +0900

    update for October 01 2020

--- crawling.py
+++ crawling.py
@@ -57,7 +57,12 @@ for i in range(len(linkList)): # list.txt 파일에 있는 각 질병관리본�
     print(valueList[80:])
 
     if int(time) >= 20072000: # 2020.07.20 00시 이후의 data 이면
-        if time == '20092000': # 2020.09.20 00시의 data 이면
+        if time == '20093000': # 2020.09.30 00시의 data 이면
+            val0 = valueList[42] + valueList[73]
+            val1 = int(str(valueList[88]) + str(valueList[89]))
+            val2 = int(str(valueList[90]) + str(valueList[91]))
+            val3 = valueList[93]
+        elif time == '20092000': # 2020.09.20 00시의 data 이면
             val0 = valueList[38] + valueList[73]
             val1 = valueList[88]
             val2 = valueList[89]
@@ -89,7 +94,7 @@ for i in range(len(linkList)): # list.txt 파일에 있는 각 질병관리본�
             val3 = valueList[85]
         elif (time == '20082100' or time == '20083000' or time == '20083100' or time == '20090100' or time == '20091200' or
               time == '20091400' or time == '20091500' or time == '20091600' or time == '20092100' or time == '20092200' or
-              time == '20092300' or time == '20092400' or time == '20092500' or time == '20092700' or time == '20092800' or time == '20092900' or time == '20093000'):
+              time == '20092300' or time == '20092400' or time == '20092500' or time == '20092700' or time == '20092800' or time == '20092900' or time == '20100100'):
             val0 = valueList[39] + valueList[70]
             val1 = valueList[85]
             val2 = valueList[86]
