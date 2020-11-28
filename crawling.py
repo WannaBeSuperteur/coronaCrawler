2ba35745adbbaa53777c4e5fec7f2fc21f64f86d
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2020-11-28 13:54:59 +0900

    update for November 28 2020

--- crawling.py
+++ crawling.py
@@ -57,7 +57,12 @@ for i in range(len(linkList)): # list.txt 파일에 있는 각 질병관리본�
     print(valueList[80:])
 
     if int(time) >= 20072000: # 2020.07.20 00시 이후의 data 이면
-        if time == '20112600': # 2020.11.26 00시의 data 이면
+        if time == '20112800': # 2020.11.28 00시의 data 이면
+            val0 = valueList[38] + valueList[67]
+            val1 = valueList[88]
+            val2 = valueList[89]
+            val3 = valueList[91]
+        elif time == '20112600': # 2020.11.26 00시의 data 이면
             val0 = int(valueList[38]) + valueList[67]
             val1 = valueList[112]
             val2 = valueList[113]
