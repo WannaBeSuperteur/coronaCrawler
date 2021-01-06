4712a9375b89c93acfb73cf7e0bd60490f35674e
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-01-06 09:55:32 +0900

    update for January 06 2021

--- crawling.py
+++ crawling.py
@@ -112,7 +112,7 @@ for i in range(len(linkList)): # list.txt 파일에 있는 각 질병관리청 �
             val1 = valueList[90]
             val2 = valueList[91]
             val3 = valueList[93]
-        elif time == '20120200' or time == '20120600' or time == '20122100' or time == '20122300' or time == '20122800':
+        elif time == '20120200' or time == '20120600' or time == '20122100' or time == '20122300' or time == '20122800' or time == '21010600':
             val0 = int(valueList[38]) + valueList[67]
             val1 = valueList[94]
             val2 = valueList[95]
