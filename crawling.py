e6dcade62f5a17b54d65f5f102e291b8dc77ef67
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-03-28 16:31:44 +0900

    update for March 28 2021

--- crawling.py
+++ crawling.py
@@ -258,7 +258,8 @@ for i in range(len(linkList)): # list.txt 파일에 있는 각 질병관리청 �
             val2 = valueList[96]
             val3 = valueList[98]
         elif (time == '20110500' or time == '20111200' or time == '20120300' or time == '20121700' or time == '20122000' or
-              time == '21021200' or time == '21021500' or time == '21030300' or time == '21030400' or time == '21031300' or time == '21031800' or time == '21032700'):
+              time == '21021200' or time == '21021500' or time == '21030300' or time == '21030400' or time == '21031300' or
+              time == '21031800' or time == '21032700' or time == '21032800'):
             val0 = int(valueList[38]) + valueList[67]
             val1 = valueList[93]
             val2 = valueList[94]
