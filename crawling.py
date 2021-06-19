9e72e44021d8d3a46cc1925b0955972ed4d8eca3
Hong Sik Kim <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-06-19 23:37:34 +0900

    update for June 19 2021

--- crawling.py
+++ crawling.py
@@ -65,7 +65,12 @@ for i in range(len(linkList)): # list.txt 파일에 있는 각 질병관리청 �
         print(valueList[i*20:(i+1)*20])
 
     if int(time) >= 20072000: # 2020.07.20 00시 이후의 data 이면
-        if time == '21061800': # 2021.06.18 00시의 data 이면
+        if time == '21061900': # 2021.06.19 00시의 data 이면
+            val0 = valueList[201] + valueList[230]
+            val1 = valueList[257]
+            val2 = valueList[258]
+            val3 = valueList[260]
+        elif time == '21061800': # 2021.06.18 00시의 data 이면
             val0 = valueList[69] + valueList[98]
             val1 = valueList[123]
             val2 = valueList[124]
